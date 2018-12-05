643bccceffe40e6fa17e25c8fbc47938c40b2866
yangyxd <yangyxd@126.com>
2018-12-05 16:37:13 +0800

    .

--- lib/flutter_picker.dart
+++ lib/flutter_picker.dart
@@ -1,10 +1,2 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 export 'Picker.dart';
 export 'PickerLocalizationsDelegate.dart';
-
-class FlutterPicker {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_picker');
-}
