c83678a6989b18a02d45d5345d5c9f3ba2275d01
yangyxd <yangyxd@126.com>
2019-06-28 13:47:08 +0800

    1.0.13

--- lib/Picker.dart
+++ lib/Picker.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/dialog.dart' as Dialog;
+import 'package:flutter/material.dart' as Dialog;
 
 const bool __printDebug = false;
 
